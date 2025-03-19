4d5909c208308735bfc61559362d70d0d52bf490
Ole-Kristian <olemofo@gmail.com>
2025-03-19 19:11:11 +0100

    methods to check and acvii art

--- src/main/java/com/oving5/Card.java
+++ src/main/java/com/oving5/Card.java
@@ -16,6 +16,53 @@ public class Card {
         return suit;
     }
 
+    /*
+     * Returns a string array with the suit image and value of the card
+     * @param suit: char, the suit of the card
+     * @param value: int, the value of the card
+     */
+    public static String[] getCardStringDraw(char suit, int value) {
+        return new String[]{suitToString(suit), String.valueOf(value)};
+    }
+
+    /*
+     * Method to convert the suit to a string as an image
+     * @param suit: char, the suit of the card
+     */
+    public static String suitToString(char suit) {
+        switch (suit) {
+            case 'H':
+                return "<3";
+            case 'D':
+                return "<>";
+            case 'S':
+                return "|->";
+            case 'C':
+                return "-3";
+            default:
+                return "Invalid suit";
+        }
+    }
+
+    /*
+     * Method to convert the value to a string
+     * @param value: int, the value of the card
+     */
+    public static String valueToString(int value) {
+        switch (value) {
+            case 1:
+                return "A";
+            case 11:
+                return "J";
+            case 12:
+                return "Q";
+            case 13:
+                return "K";
+            default:
+                return String.valueOf(value);
+        }
+    }
+
     public static char[] getSuits() {
         return new char[]{'H', 'D', 'S', 'C'};
     }
@@ -39,6 +86,6 @@ public class Card {
 
     @Override
     public String toString() {
-        return suit + " " + value;
+        return String.valueOf(suit) + value;
     }
 }
--- src/main/java/com/oving5/Hand.java
+++ src/main/java/com/oving5/Hand.java
@@ -1,9 +1,19 @@
 package com.oving5;
 
+import java.util.ArrayList;
+
 public class Hand extends Deck {
 
     public Hand() {
         super(false);
+    }
+
+    public int getHandSize() {
+        return getCards().size();
+    }
+
+    public void setHandFill(ArrayList<Card> cards) {
+        getCards().addAll(cards);
     }
 
     public boolean isFlush(int threshold) {
@@ -23,5 +33,53 @@ public class Hand extends Deck {
 
         return false;
     }
+
+    public boolean isStraight(int threshold) {
+
+        for(Card cards : getCards()) {
+            int count = 0;
+            for(Card card : getCards()) {
+                if(cards.getValue() == card.getValue() + 1) {
+                    count++;
+                }
+            }
+            if(count >= threshold) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
+    public int getCountSpesificValue(int value) {
+        return (int) getCards().stream()
+                .filter(card -> card.getValue() == value)
+                .count();
+    }
+
+    public int getCountSpesificSuit(char suit) {
+        return (int) getCards().stream()
+                .filter(card -> card.getSuit() == suit)
+                .count();
+    }
+
+    public ArrayList<Card> getSpesificSuit(char suit) {
+        ArrayList<Card> cards = new ArrayList<>();
+        getCards().stream()
+                .filter(card -> card.getSuit() == suit)
+                .forEach(c -> cards.add(c));
+        return cards;
+    }
+
+    public void printSpesificSuit(char suit) {
+        StringBuilder printString = new StringBuilder();
+        getCards().stream()
+                .filter(card -> card.getSuit() == suit)
+                .forEach(c -> printString.append(c.toString()).append(", "));
+    }
+
+
 
 }
